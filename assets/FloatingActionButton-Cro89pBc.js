e25808ffe44b8b6b9742e976ce28f02625a523ac
Divyesh <limbasiyadivyesh5201@gmal.com>
2026-06-16 17:29:54 +0530

    web P latest

--- assets/FloatingActionButton-Cro89pBc.js
+++
@@ -1,6 +0,0 @@
-import{j as e}from"./editorVendor-D_Lx4EXd.js";import{m as t}from"./uiVendor-ZYlIWZ6Q.js";const n="/img/app/add.svg",s=({onClick:i=()=>{},iconSrc:a=n})=>e.jsx(t.div,{className:"fixed bottom-6 right-6 z-50",children:e.jsxs(t.button,{onClick:i,whileHover:{scale:1.15,rotate:2},whileTap:{scale:.88},initial:{opacity:0,y:40},animate:{opacity:1,y:0},transition:{type:"spring",stiffness:300,damping:20},className:`group relative flex items-center justify-center w-[50px] h-[50px] rounded-lg\r
-                animate-bounce\r
-               bg-gradient-to-br from-indigo-600 via-blue-600 to-cyan-500\r
-               shadow-[0_12px_30px_rgba(0,0,0,0.25)]\r
-               hover:shadow-[0_18px_45px_rgba(0,0,0,0.35)]\r
-               transition-all duration-300 overflow-hidden`,children:[e.jsx(t.div,{whileHover:{rotate:90},transition:{duration:.4},className:"relative z-10 flex items-center justify-center",children:e.jsx("img",{src:a,alt:"action",className:"w-6 h-6 drop-shadow-lg"})}),e.jsx("span",{className:"absolute inset-0 rounded-full border border-white/50 pointer-events-none"})]})});export{s as F};
